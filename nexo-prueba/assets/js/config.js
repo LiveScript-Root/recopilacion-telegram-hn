af0118975d98d886ade37cc92bda9cb83591ad98
LiveScript-Root <dispositivoshomee@gmail.com>
2026-09-18 22:39:28 -0700

    Add NEXO test asset

---
+++ nexo-prueba/assets/js/config.js
@@ -0,0 +1,12 @@
+window.SITE_CONFIG = Object.freeze({
+  brand: "NEXO",
+  domain: "https://livescript-root.github.io/recopilacion-telegram-hn/nexo-prueba",
+  supportEmail: "soportepagoseguros@gmail.com",
+  paypalAmount: "12.00",
+  paypalCurrency: "USD",
+  paypalHostedButtonId: "WCC2TYW6EW2R2",
+  paypalDirectUrl: "https://www.paypal.com/ncp/payment/WCC2TYW6EW2R2",
+  paypalClientId: "BAAwAne86stalbO5OCSqv60EmOd1Heu9PbcuLS-DrsCBTpGbm5eaTUluXANof70izj9uhcv9lZKcDSuqRs",
+  returnUrl: "https://livescript-root.github.io/recopilacion-telegram-hn/nexo-prueba/solicitud-recibida/",
+  apiBase: "/api"
+});
